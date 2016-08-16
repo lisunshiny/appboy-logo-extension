111d8c76457d9538bd8f47420ca51c0fb11ace4c
Liann Sun <lisunshiny@gmail.com>
2016-08-15 22:58:58 -0400

    add timeout

--- changecolor.js
+++ changecolor.js
@@ -1,13 +1,24 @@
+var timeWaiting = 0;
+
 function waitForElementToDisplay(selector, time) {
   if(document.querySelector(selector)!=null) {
     var el = document.getElementsByClassName('gb_Sb')[0];
-    el.style.backgroundImage = "url(\"https://content.appboy.com/uploads/pQjGgL2ko88OLfAn_Hr0cALogo_White_324x80_2x.png\")"
-    el.style.marginTop = "12px"
+    el.style.backgroundImage = "url(\"https://content.appboy.com/uploads/pQjGgL2ko88OLfAn_Hr0cALogo_White_324x80_2x.png\")";
+    el.style.marginTop = "12px";
     return;
   }
   else {
     setTimeout(function() {
-      waitForElementToDisplay(selector, time);
+      timeWaiting += time;
+
+      // If waiting for more than 10 seconds, abort
+      if (timeWaiting > 10000) {
+        console.log("Couldn't find logo element :(");
+        return;
+      }
+      else {
+        waitForElementToDisplay(selector, time);
+      }
     }, time);
   }
 }
